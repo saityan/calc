2509e0f9805f1c5b3f8e1ac6b70f331cd9d93cb2
saityan <naminatosky@gmail.com>
2021-08-06 01:49:43 +0300

    added computation for multiplication

--- app/src/main/java/ru/geekbrains/calc/Data.java
+++ app/src/main/java/ru/geekbrains/calc/Data.java
@@ -1,5 +1,9 @@
 package ru.geekbrains.calc;
 
+import android.util.Log;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 class Data {
     private StringBuilder expression;
     private String memory;
@@ -29,7 +33,34 @@ class Data {
     }
 
     void compute() {
+        if (!isExpressionEmpty()) {
+            LinkedList <String> sample = new LinkedList<>(Arrays.asList(this.getExpression().split(
+                    "(?<=[\\d.])(?=[^\\d.])|(?<=[^\\d.])(?=[\\d.])")));
+            if (sample.size() < 3)
+                return;
+            if (sample.get(sample.size() - 1).matches("[*/+-]"))
+                sample.removeLast();
+            while(sample.size() > 1) {
+                for (int i = 0; i < sample.size(); i ++) {
+                    if (sample.get(i).matches("[*/]")) {
+                        double a = Double.parseDouble(sample.get(i - 1));
+                        double b = Double.parseDouble(sample.get(i + 1));
+                        double result;
+                        if (sample.get(i).equals("*")) {
+                            result = a * b;
+                            sample.set(i - 1, String.valueOf(result));
+                            sample.remove(i + 1);
+                            sample.remove(i);
+                            continue;
+                        }
+                    }
+                }
+            }
+            this.expression.setLength(0);
+            this.expression.append(sample.get(0));
 
+            Log.d("result", sample.toString());
+        }
     }
 
     void clear() { this.expression.setLength(0); }
@@ -54,8 +85,8 @@ class Data {
     }
 
     private boolean isPointEligible() {
-        String sample = this.getExpression();
-        if (sample.length() > 0) {
+        if (!isExpressionEmpty()) {
+            String sample = this.getExpression();
             if (sample.lastIndexOf('*') > sample.lastIndexOf('.'))
                 return true;
             if (sample.lastIndexOf('+') > sample.lastIndexOf('.'))

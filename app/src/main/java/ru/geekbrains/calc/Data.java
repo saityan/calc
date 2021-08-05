5eff05ac3b727ea9d43ba427b2456c7cfafd05f3
saityan <naminatosky@gmail.com>
2021-08-05 23:37:27 +0300

    added input restrictions

--- app/src/main/java/ru/geekbrains/calc/Data.java
+++ app/src/main/java/ru/geekbrains/calc/Data.java
@@ -10,8 +10,13 @@ class Data {
     }
 
     void add(String s) {
-        if (s.length() > 0 && this.expression.length() < 32)
+        if (s.length() > 0 && this.expression.length() < 32) {
+            if (!isDigitLast() && s.matches("[*/+.-]"))
+                return;
+            if (s.equals(".") && !isPointEligible())
+                return;
             this.expression.append(s);
+        }
     }
 
     String getExpression () { return this.expression.toString(); }
@@ -23,7 +28,9 @@ class Data {
             this.memory = memory;
     }
 
-    void compute() { /*add regex here*/ }
+    void compute() {
+
+    }
 
     void clear() { this.expression.setLength(0); }
 
@@ -35,5 +42,31 @@ class Data {
     void backspace() {
         if (this.expression.length() > 0)
             this.expression.setLength(this.expression.length() - 1);
+    }
+
+    private boolean isExpressionEmpty() {
+        return this.getExpression().length() == 0;
+    }
+
+    private boolean isDigitLast() {
+        return !isExpressionEmpty() && this.getExpression().substring(
+                this.getExpression().length() - 1).matches("[0-9]");
+    }
+
+    private boolean isPointEligible() {
+        String sample = this.getExpression();
+        if (sample.length() > 0) {
+            if (sample.lastIndexOf('*') > sample.lastIndexOf('.'))
+                return true;
+            if (sample.lastIndexOf('+') > sample.lastIndexOf('.'))
+                return true;
+            if (sample.lastIndexOf('-') > sample.lastIndexOf('.'))
+                return true;
+            if (sample.lastIndexOf('/') > sample.lastIndexOf('.'))
+                return true;
+            return !sample.contains("*") && !sample.contains("+") &&
+                    !sample.contains("-") && !sample.contains("/") && !sample.contains(".");
+        }
+        return false;
     }
 }
